568889ef9204e965f69dc12ad66d37bc979a75af
logicfeel <logicfeel@nate.com>
2023-03-10 15:37:46 +0900

    커버리지 깃 테스트

--- __test__/clear.js
+++ __test__/clear.js
@@ -8,7 +8,7 @@ const dirname2   = __dirname + "/pageGroup/mod1";
 const dirname3   = __dirname + "/import/mod1";
 const dirname4   = __dirname + "/extends/mod1";
 
-describe.only("task :: clear", () => {
+describe("task :: clear", () => {
     it("[ base 생성 및 do_clear(1) ]", () => {
         autoTask = AutoTask.create(dirname1);
         autoTask.isLog = false;
